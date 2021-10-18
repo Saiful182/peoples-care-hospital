574e1ccf9496e68ba4fb5705e16b5958dfa6e4a4
Saiful182 <saifulislamcse0@gamil.com>
2021-10-18 22:58:28 +0600

    service added

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -3,6 +3,8 @@ import useServices from '../hooks/useServices';
 
 import Banner from './Banner/Banner';
 import HomeService from './Service/HomeService';
+import './Home.css';
+import { Row } from 'react-bootstrap';
 
 const Home = () => {
     const [services] = useServices();
@@ -12,13 +14,16 @@ const Home = () => {
     console.log(sixServices.length);
     return (
         <div>
+
             <Banner></Banner>
-            <div className="">
-                {
-                    sixServices.map(service =>
-                        <HomeService service={service} id={service.id}></HomeService>
-                    )
-                }
+            <div>
+                <Row xs={1} md={2} lg={3}>
+                    {
+                        sixServices.map(service =>
+                            <HomeService service={service} id={service.id}></HomeService>
+                        )
+                    }
+                </Row>
             </div>
         </div>
     );
--- src/component/Home/Service/HomeService.js
+++ src/component/Home/Service/HomeService.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 
 const HomeService = (props) => {
-    let { name } = props.service;
+    let { name, img, Room, description, stuff } = props.service;
     console.log(name);
     return (
-        <div>
-            <h1>hellow wolrd{name}</h1>
-        </div>
+
+        <Card>
+            <Card.Img variant="top" src={img} />
+            <Card.Body>
+                <Card.Title>{name}</Card.Title>
+                <Card.Text>
+                    Room No: {Room}
+                </Card.Text>
+                <Card.Text>
+                    Stuff deducated: {stuff}
+                </Card.Text>
+                <Card.Text>
+                    {description}
+                </Card.Text>
+
+            </Card.Body>
+
+        </Card>
+
     );
 };
 
